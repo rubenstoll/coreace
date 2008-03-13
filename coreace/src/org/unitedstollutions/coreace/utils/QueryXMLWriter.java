753692a9b06cdbbed55f2fd5f2114c2e707ff060
rubenstoll <rubenstoll@hotmail.com>
2008-03-13 19:00:43 +0000

    added writeOneIFC method to xml query writer in order to write out one xml query containing all the concepts in one.

--- coreace/src/org/unitedstollutions/coreace/utils/QueryXMLWriter.java
+++ coreace/src/org/unitedstollutions/coreace/utils/QueryXMLWriter.java
@@ -74,6 +74,62 @@ public class QueryXMLWriter {
 	}
 
 	/**
+	 * Writes all the Query File's IFC lists to ONE xml file.
+	 * 
+	 * @param queryFileList
+	 */
+	public void writeOneIFC(ArrayList<QueryFile> queryFileList) {
+
+		// define an output directory
+		File outputDir = new File("output");
+		if(outputDir.mkdir()) {
+			// only goes in once - upon directory creation
+			System.out.println("Created output directory ...");
+		}
+		
+		Element root = new Element("query");
+//		Element filename = new Element("filename");
+//		filename.setText(qf.getFileName());
+//		root.addContent(filename);
+		Element concepts = new Element("concepts");
+		root.addContent(concepts);
+
+		for (QueryFile qf : queryFileList) {
+
+			for (String currConcept : qf.getIfcList()) {
+				Element concept = new Element("concept");
+				// concept.setAttribute("index", String.valueOf(i));
+				concept.setText(currConcept);
+				concepts.addContent(concept);
+			}
+
+		}
+
+		Document doc = new Document(root);
+
+		// write to the output file with directory specified
+		// NOTE: the .txt from the file name is dropped and replaced by .xml
+
+		// set the file with directory prepended
+		String outputFileName = outputDir.toString() + File.separator
+				+  "queries.xml";
+
+		// start writing
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(
+					outputFileName));
+			XMLOutputter serializer = new XMLOutputter(Format
+					.getPrettyFormat());
+			serializer.output(doc, out);
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+
+		System.out.println("Done!");
+		System.out.println("Output written to directory: " + outputDir);
+	}	
+	
+	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -91,6 +147,10 @@ public class QueryXMLWriter {
 		ArrayList<QueryFile> qryFileList = qp.parse(qp.getQueryFiles(), dir);
 
 		qwr.writeIFC(qryFileList);
+		
+		// now write one big one
+		qwr.writeOneIFC(qryFileList);
+		
 
 	}
 
